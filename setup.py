c63984418f0c478bd695bf3c159363d96514882d
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2013-03-16 00:26:14 +0000

    Added setup classifiers

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # Get the version number from the package
 f = open('ZPsycopgDA/__init__.py')
@@ -30,7 +30,11 @@ setup(name='ZPsycopgDA',
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Topic :: Database",
         ],
       keywords='',
       author='Federico Di Gregorio',

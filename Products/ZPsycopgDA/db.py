6d61e349c6cd5626b86b7be573177db57343506f
Ján Jockusch <jan.jockusch@perfact.de>
2018-10-09 12:49:48 +0200

    Major recode of db.py without strange retries.

--- Products/ZPsycopgDA/db.py
+++ Products/ZPsycopgDA/db.py
@@ -18,23 +18,69 @@
 from Shared.DC.ZRDB.TM import TM
 from Shared.DC.ZRDB import dbi_db
 
-from ZODB.POSException import ConflictError
-
-# Python2 backward compatibility
-try:
-    from . import pool
-except SyntaxError:
-    import pool
-
 import psycopg2
-from psycopg2.extensions import INTEGER, LONGINTEGER, FLOAT, BOOLEAN, DATE, TIME
-from psycopg2.extensions import TransactionRollbackError, register_type
+from psycopg2.extensions import INTEGER, LONGINTEGER, BOOLEAN, DATE, TIME
+from psycopg2.extensions import register_type
 from psycopg2 import NUMBER, STRING, ROWID, DATETIME
+from psycopg2.pool import AbstractConnectionPool, ThreadedConnectionPool
 
 from logging import getLogger
 LOG = getLogger('ZPsycopgDA.db')
 
 # the DB object, managing all the real query work
+
+
+class CustomConnectionPool(ThreadedConnectionPool):
+
+    def _connect(self, key=None):
+        """Patched version of AbstractConnectionPool._connect(), which adds
+        isolation levels, read-only mode, and encoding settings."""
+        # Only use the 'dsn' keyword argument
+        LOG.warning("Making a new connection to PostgreSQL "
+                    "for pool %s, key %s" % (repr(self), repr(key)))
+        args = self._kwargs
+        conn = psycopg2.connect(dsn=args['dsn'])
+
+        # Patch JJ 2016-05-05: This is the moment to set the correct
+        # transaction isolation level, encoding, and types.
+        conn.set_session(isolation_level=int(args['tilevel']),
+                         readonly=bool(args['readonlymode']))
+        if 'encoding' in args:
+            conn.set_client_encoding(args['encoding'])
+        if 'typecasts' in args:
+            for tc in args['typecasts']:
+                register_type(tc, conn)
+
+        # The following code is identical to the code in
+        # AbstractConnectionPool
+        if key is not None:
+            self._used[key] = conn
+            self._rused[id(conn)] = key
+        else:
+            self._pool.append(conn)
+        return conn
+
+    def _getkey(self):
+        """Return the thread identifier as a key."""
+        try:
+            import thread
+            threading = thread
+        except ImportError:
+            import threading
+        key = threading.get_ident()
+        return key
+
+    def _putconn(self, conn, key=None, close=False):
+        """Patched version of AbstractConnectionPool._putconn(), which closes
+        the connection only if 'close' is set, not if pool size is greater
+        than minimum or 'close' is set."""
+        stored_minconn = self.minconn
+        self.minconn = 10  # magic number: number of connections to keep
+        retval = AbstractConnectionPool._putconn(
+            self, conn=conn, key=key, close=close)
+        self.minconn = stored_minconn
+        return retval
+
 
 class DB(TM, dbi_db.DB):
 
@@ -43,15 +89,6 @@ class DB(TM, dbi_db.DB):
     def __init__(self, dsn, tilevel, typecasts, enc='utf-8',
                  autocommit=False, readonlymode=False, physical_path=''):
         self.dsn = dsn
-
-        # Patch JJ 2016-05-05: Using the dsn as a key for connection
-        # pooling mixes connections with different transaction
-        # isolation levels, leading to very confusing and dangerous
-        # errors. We need a better key, consisting of all
-        # variables. This key will be used by pool.py
-        self.key = (physical_path or
-                    str((dsn, tilevel, enc, typecasts, autocommit, readonlymode)))
-
         self.tilevel = tilevel
         self.typecasts = typecasts
         if enc is None or enc == "":
@@ -70,82 +107,28 @@ class DB(TM, dbi_db.DB):
 
         self.make_mappings()
 
+        self.pool = CustomConnectionPool(
+            # 100 = sufficiently high magic number > max number of threads
+            minconn=0, maxconn=100,
+            dsn=self.dsn, tilevel=self.tilevel, typecasts=self.typecasts,
+            readonlymode=self.readonlymode)
+
     def getconn(self, init=True):
-        retries = 10 # Magic number
-        while True:
-            try:
-                conn = self.getconn_inner(init)
-                break
-            except (psycopg2.OperationalError, psycopg2.InterfaceError):
-                LOG.info('Connect error. Retries: %d' % retries)
-                self.putconn(True)
-                retries -= 1
-                # DANGER: time.sleep() does not play well in multithreading.
-                #import time
-                #time.sleep(0.2) # Magic number
-                if retries == 0:
-                    raise
-        return conn
-
-    def getconn_inner(self, init=True):
-        # if init is False we are trying to get hold on an already existing
-        # connection, so we avoid to (re)initialize it risking errors.
-
-        # Patch JJ 2016-05-05: The pool needs to know the tilevel,
-        # encoding and typecasts as well.
-        conn = pool.getconn(self.dsn,
-                            key=self.key, tilevel=self.tilevel,
-                            readonlymode=self.readonlymode,
-                            encoding=self.encoding, typecasts=self.typecasts)
-
-        if init:
-            # Patch JJ 2014-05-27: Make sure you begin a new
-            # transaction.
-            try:    tainted = self.rollback_tainted
-            except: tainted = False
-            if tainted: conn.rollback()
-            else:       conn.commit()
-            self.rollback_tainted = False
-
-            # use set_session where available as in these versions
-            # set_isolation_level generates an extra query.
-            if psycopg2.__version__ >= '2.4.2':
-                conn.set_session(isolation_level=int(self.tilevel),
-                                 readonly=bool(self.readonlymode))
-            else:
-                conn.set_isolation_level(int(self.tilevel))
-            conn.set_client_encoding(self.encoding)
-            for tc in self.typecasts:
-                register_type(tc, conn)
+        conn = self.pool.getconn()
         return conn
 
     def putconn(self, close=False):
-        try:
-            # Patch JJ 2016-05-05: The pool needs to know the tilevel,
-            # encoding and typecasts as well.
-            conn = pool.getconn(self.dsn, False,
-                                key=self.key, tilevel=self.tilevel,
-                                readonlymode=self.readonlymode,
-                                encoding=self.encoding, typecasts=self.typecasts)
-        except AttributeError:
-            pass
-        # Patch JJ 2016-05-05: The pool needs to know the tilevel,
-        # encoding and typecasts as well.
-        pool.putconn(self.dsn, conn, close,
-                     key=self.key, tilevel=self.tilevel,
-                     readonlymode=self.readonlymode,
-                     encoding=self.encoding, typecasts=self.typecasts)
+        conn = self.pool.getconn()
+        return self.pool.putconn(conn, close=close)
 
     def getcursor(self):
-        conn = self.getconn(False)
-        # Patch JJ 2016-11-03: Catch lost connection situations
+        conn = self.pool.getconn()
         try:
             cursor = conn.cursor()
         except psycopg2.InterfaceError:
-            LOG.warning('Found interface error in getcursor(). Reconnecting')
-            self.putconn(True)
-            conn = self.getconn(True)
-            cursor = conn.cursor()
+            # Connection is broken. Put away, then raise.
+            self.pool.putconn(close=True)
+            raise
         return cursor
 
     def _commit(self, put_connection=False):
@@ -156,11 +139,15 @@ class DB(TM, dbi_db.DB):
             # tainted by one of the statements executed. If that is
             # the case, we must roll back and raise an error.
 
-            try:    tainted = conn.rollback_tainted
-            except: tainted = False
+            try:
+                tainted = conn.rollback_tainted
+            except AttributeError:
+                tainted = False
 
-            if tainted: conn.rollback()
-            else:       conn.commit()
+            if tainted:
+                conn.rollback()
+            else:
+                conn.commit()
 
             self.rollback_tainted = False
             if put_connection:
@@ -204,21 +191,10 @@ class DB(TM, dbi_db.DB):
             pass
 
     def open(self):
-        # this will create a new pool for our DSN if not already existing,
-        # then get and immediately release a connection
-        self.getconn()
-        self.putconn()
+        pass
 
     def close(self):
-        # FIXME: if this connection is closed we flush all the pool associated
-        # with the current DSN; does this makes sense?
-
-        # Patch JJ 2016-05-05: The pool needs to know the tilevel,
-        # encoding and typecasts as well.
-        pool.flushpool(self.dsn,
-                       key=self.key, tilevel=self.tilevel,
-                       readonlymode=self.readonlymode,
-                       encoding=self.encoding, typecasts=self.typecasts)
+        pass
 
     def sortKey(self):
         return 1
@@ -247,7 +223,7 @@ class DB(TM, dbi_db.DB):
             })
         return items
 
-    ## tables and rows ##
+    # tables and rows
 
     def tables(self, rdb=0, _care=('TABLE', 'VIEW')):
         self._register()
@@ -278,7 +254,7 @@ class DB(TM, dbi_db.DB):
         self.putconn()
         return self.convert_description(c.description, True)
 
-    ## query execution ##
+    # query execution
 
     def query(self, query_string, max_rows=None, query_data=None):
         self._register()
@@ -291,87 +267,65 @@ class DB(TM, dbi_db.DB):
         c = self.getcursor()
 
         try:
-            retries = 10 # Magic number
             for qs in [x for x in query_string.split('\0') if x]:
-                while True:
-                    try:
-                        #LOG.info("Trying to execute statement %s" % qs)
-                        if query_data:
-                            c.execute(qs, query_data)
-                        else:
-                            c.execute(qs)
-                        if self.autocommit:
-                            # LOG.info('Autocommitting %s' % qs)
-                            self._commit()
-                    except TransactionRollbackError as err:
-                        LOG.warning('Transaction Rollback!')
-                        # Patch JJ 2016-05-03: Transaction Rollbacks
-                        # should not be raised as ConflictErrors, because
-                        # these trigger Request Retries (up to four
-                        # times!) Instead, raise the error.
-                        raise err
+                try:
+                    # LOG.info("Trying to execute statement %s" % qs)
+                    if query_data:
+                        c.execute(qs, query_data)
+                    else:
+                        c.execute(qs)
+                    if self.autocommit:
+                        # LOG.info('Autocommitting.')
+                        self._commit()
 
-                        # Ha, here we have to look like we are the ZODB raising conflict errrors, raising ZPublisher.Publish.Retry just doesn't work
-                        #logging.debug("Serialization Error, retrying transaction", exc_info=True)
-                        #raise ConflictError("TransactionRollbackError from psycopg2")
+                except psycopg2.OperationalError as err:
+                    connection_closed_error = (
+                        'server closed the connection' in repr(err) or
+                        'could not connect to server' in repr(err) or
+                        'the database system is shutting down' in repr(err) or
+                        'the database system is starting up' in repr(err))
+                    if connection_closed_error:
+                        # Close all connections in the pool if there
+                        # was a connection problem.
+                        LOG.error("Operational error on connection, "
+                                  "closing all in this pool: %s." %
+                                  repr(self.pool))
+                        for key, conn in self.pool._used.items():
+                            self.pool.putconn(conn=conn, key=key, close=True)
 
-                    except psycopg2.OperationalError as err:
-                        LOG.error("Operational error on connection, closing it.")
-                        try:
-                            # Only close our connection
-                            self.putconn(True)
-                        except:
-                            #LOG.debug("Something went wrong when we tried to close the pool", exc_info=True)
-                            pass
+                    # Patch JJ 2014-05-23: Operational errors should
+                    # not be muted! This masks transaction rollbacks
+                    # in a very ugly way. Instead, raise the error.
+                    LOG.info('Passing through operational error')
+                    raise err
 
-                        # JJ 2017-08-07: Do not retry canceled queries
-                        if repr(err).startswith('QueryCanceledError'):
-                            retries = 0
+                if c.description is not None:
+                    nselects += 1
+                    if c.description != desc and nselects > 1:
+                        raise psycopg2.ProgrammingError(
+                            'multiple selects in single query not allowed')
+                    if max_rows:
+                        res = c.fetchmany(max_rows)
+                        # JJ 2017-07-20: Danger ahead. We might
+                        # have many more rows in the database,
+                        # which are truncated by max_rows. In that
+                        # case, we should be able to react, by
+                        # raising or logging.
+                        if len(res) == max_rows:
+                            try:
+                                overshoot_result = c.fetchone()
+                            except:
+                                overshoot_result = None
+                            if overshoot_result:
+                                assert False, (
+                                    "This query has returned more than "
+                                    "max_rows results. Please raise "
+                                    "max_rows or limit in SQL.")
 
-                        # Patch JJ 2016-11-03: And immediately reopen it
-                        if retries:
-                            LOG.warning("Reopening the connection, retries: %d" % retries)
-                            retries -= 1
-                            self.getconn(True)
-                            c = self.getcursor()
-                            continue
+                    else:
+                        res = c.fetchall()
+                    desc = c.description
 
-                        # Patch JJ 2014-05-23: Operational errors should
-                        # not be muted! This masks transaction rollbacks
-                        # in a very ugly way. Instead, raise the error.
-                        LOG.info('Passing through operational error')
-                        raise err
-
-                    if c.description is not None:
-                        nselects += 1
-                        if c.description != desc and nselects > 1:
-                            raise psycopg2.ProgrammingError(
-                                'multiple selects in single query not allowed')
-                        if max_rows:
-                            res = c.fetchmany(max_rows)
-                            # JJ 2017-07-20: Danger ahead. We might
-                            # have many more rows in the database,
-                            # which are truncated by max_rows. In that
-                            # case, we should be able to react, by
-                            # raising or logging.
-                            if len(res) == max_rows:
-                                try:
-                                    overshoot_result = c.fetchone()
-                                except:
-                                    overshoot_result = None
-                                if overshoot_result:
-                                    assert False, (
-                                        "This query has returned more than "
-                                        "max_rows results. Please raise "
-                                        "max_rows or limit in SQL.")
-
-                        else:
-                            res = c.fetchall()
-                        desc = c.description
-
-                    # No retries on subsequent queries
-                    retries = 0
-                    break
             self.failures = 0
 
         except Exception as err:

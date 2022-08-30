e0c539d04ee79ca99ae0e47dfbe1f510f271b715
Jens Vagelpohl <jens@plyp.com>
2022-08-30 21:26:39 +0000

    - update copyright headers where code is entirely new [ci skip]

--- src/Products/ZPsycopgDA/tests/utils.py
+++ src/Products/ZPsycopgDA/tests/utils.py
@@ -1,26 +1,15 @@
-# testutils.py - utility module for psycopg2 testing.
-
+##############################################################################
 #
-# Copyright (C) 2010-2011 Daniele Varrazzo  <daniele.varrazzo@gmail.com>
+# Copyright (c) 2022 Zope Foundation and Contributors.
 #
-# psycopg2 is free software: you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
 #
-# In addition, as a special exception, the copyright holders give
-# permission to link this program with the OpenSSL library (or with
-# modified versions of OpenSSL that use the same license as OpenSSL),
-# and distribute linked combinations including the two.
-#
-# You must obey the GNU Lesser General Public License in all respects for
-# all of the code used other than OpenSSL.
-#
-# psycopg2 is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
-# License for more details.
-
+##############################################################################
 
 import psycopg2
 

e7c19e27a58db2146e22f6f779b4e5c4e09002fe
Jens Vagelpohl <jens@plyp.com>
2022-08-31 07:12:18 +0000

    - move more tests

--- src/Products/ZPsycopgDA/tests/test_db.py
+++ src/Products/ZPsycopgDA/tests/test_db.py
@@ -12,9 +12,11 @@
 ##############################################################################
 """ Tests for the db module
 """
+import threading
 import time
 import unittest
 
+from ..DA import ZDATETIME
 from ..db import DB
 from . import DSN
 from . import NO_DB_MSG
@@ -85,3 +87,46 @@ class RealDBTests(unittest.TestCase):
             cur3.close()
         finally:
             conn.close()
+
+    def test_multithreaded_connection_initialization(self):
+        # test connection initialization with multiple threads
+        #
+        # from <https://github.com/psycopg/psycopg2/pull/2>:
+        # The connection initialization (transaction isolation, typecasts)
+        # was only done for first connection. When there are multiple
+        # threads running in parallel, connections where used which had not
+        # been initialized correctly.
+
+        typecasts = [ZDATETIME]
+
+        def DA_connect():
+            db = DB(DSN, tilevel=2, typecasts=typecasts)
+            db.open()
+            return db
+
+        failures = []
+
+        def assert_casts(conn, name):
+            # connection = conn.getcursor().connection
+            # See https://github.com/psycopg/psycopg2/issues/155
+            connection = conn.getconn().cursor().connection
+            if (connection.string_types !=
+                    {1114: ZDATETIME, 1184: ZDATETIME}):
+                failures.append(
+                    '%s fail (%s)' % (name, connection.string_types))
+
+        def test_connect(name):
+            assert_casts(conn1, name)
+
+        conn1 = DA_connect()
+        try:
+            t1 = threading.Thread(target=test_connect, args=('t1',))
+            t1.start()
+            t2 = threading.Thread(target=test_connect, args=('t2',))
+            t2.start()
+            t1.join()
+            t2.join()
+
+            self.assertFalse(failures)
+        finally:
+            conn1.close()
